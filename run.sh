c4c5ac72b01b91d0c7e44019b72989e345c5fe90
yamao2253 <yamao2253@gmail.com>
2015-12-20 19:45:42 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Install ansible-playbook
+test -z $(which ansible-playbook) && \
+  sudo apt-get -y install software-properties-common && \
+  sudo apt-add-repository -y ppa:ansible/ansible && \
+  sudo apt-get update && \
+  sudo apt-get -y install ansible
+
+ansible-playbook rpi.yml --connection=local
+exec $SHELL -l
